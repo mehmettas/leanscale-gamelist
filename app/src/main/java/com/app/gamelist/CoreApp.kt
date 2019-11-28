e9722996a717c8974eb570963b582e0bc5155287
mehmettas <mtas372372@hotmail.com>
2019-11-28 13:12:34 +0300

    CoreApp class that would initialize the main component responsibilities is created

---
+++ app/src/main/java/com/app/gamelist/CoreApp.kt
@@ -0,0 +1,24 @@
+package com.app.gamelist
+
+import android.app.Application
+import android.content.Context
+import org.koin.android.ext.android.startKoin
+
+class CoreApp: Application() {
+
+    companion object{
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        configureDependencyInjection()
+    }
+
+    // Place where we start the koin
+    private fun configureDependencyInjection() {
+        startKoin(this, appModule)
+    }
+
+}
