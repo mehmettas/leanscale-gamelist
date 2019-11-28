1e0bf0a77aa270853ee7c8187ae181a5b9d187d3
mehmettas <mtas372372@hotmail.com>
2019-11-28 14:04:10 +0300

    ManagerModule updated with first dependency values - Defined with Single to create instances once for whole life time of the project

--- app/src/main/java/com/app/gamelist/di/ManagerModule.kt
+++ app/src/main/java/com/app/gamelist/di/ManagerModule.kt
@@ -1,6 +1,10 @@
 package com.app.gamelist.di
 
+import com.app.gamelist.data.remote.RemoteDataManager
+import com.app.gamelist.data.repository.DataManager
 import org.koin.dsl.module.module
 
 val managerModule = module {
+    single { DataManager(get()) }
+    single { RemoteDataManager() }
 }
