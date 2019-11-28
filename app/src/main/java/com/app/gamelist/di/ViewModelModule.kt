9645180ca0ff6fb0a22229216ee4c7a395097032
mehmettas <mtas372372@hotmail.com>
2019-11-28 14:55:55 +0300

    Added first viewModelModule for dependency injection - SplashViewModel

--- app/src/main/java/com/app/gamelist/di/ViewModelModule.kt
+++ app/src/main/java/com/app/gamelist/di/ViewModelModule.kt
@@ -1,6 +1,9 @@
 package com.app.gamelist.di
 
+import com.app.gamelist.ui.splash.SplashViewModel
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val viewModelModule = module {
+    viewModel { SplashViewModel(get()) }
 }
