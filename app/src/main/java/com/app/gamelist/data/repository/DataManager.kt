3ead30026ed1b90f460b4144484490b66bf18066
mehmettas <mtas372372@hotmail.com>
2019-11-28 14:02:06 +0300

    Repository Part that implements services set with it's interface

---
+++ app/src/main/java/com/app/gamelist/data/repository/DataManager.kt
@@ -0,0 +1,10 @@
+package com.app.gamelist.data.repository
+
+import com.app.gamelist.data.remote.RemoteDataManager
+
+class DataManager(
+    private val remoteDataManager: RemoteDataManager
+):IDataManager {
+
+}
+
---
+++ app/src/main/java/com/app/gamelist/data/repository/IDataManager.kt
@@ -0,0 +1,6 @@
+package com.app.gamelist.data.repository
+
+import com.app.gamelist.data.remote.IRemoteDataManager
+
+interface IDataManager: IRemoteDataManager {
+}
