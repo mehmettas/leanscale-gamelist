1e32c7e8f1ffd476a4f1a086cdad2d6f5b8db694
mehmettas <mtas372372@hotmail.com>
2019-11-28 15:06:36 +0300

    Created a sample delay operation on Splash Page

--- app/src/main/java/com/app/gamelist/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/app/gamelist/ui/splash/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.app.gamelist.ui.splash
 
+import android.content.Intent
+import androidx.lifecycle.Observer
+import com.app.gamelist.MainActivity
 import com.app.gamelist.R
 import com.app.gamelist.ui.base.BaseActivity
+import com.app.gamelist.utils.kotlinextensions.launchActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : BaseActivity(), ISplashNavigator {
@@ -15,6 +19,16 @@ class SplashActivity : BaseActivity(), ISplashNavigator {
     }
 
     override fun initUI() {
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.isFinished.observe(this, Observer {       // Observe liveData, when it changes change the page
+            if (it)
+                launchActivity<MainActivity>{
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+        })
     }
 
     override fun initListener() {
--- app/src/main/java/com/app/gamelist/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/app/gamelist/ui/splash/SplashViewModel.kt
@@ -1,7 +1,24 @@
 package com.app.gamelist.ui.splash
 
+import android.os.CountDownTimer
+import androidx.lifecycle.MutableLiveData
 import com.app.gamelist.data.repository.DataManager
 import com.app.gamelist.ui.base.BaseViewModel
 
 class SplashViewModel(dataManager: DataManager): BaseViewModel<ISplashNavigator>(dataManager) {
+    val isFinished = MutableLiveData<Boolean>()
+
+    // As soon as class initialized, start the timer, and do a little wait for the splash
+    init {
+        object : CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                isFinished.postValue(true)
+            }
+        }.start()
+    }
+
 }
