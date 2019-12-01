ac587f9c9a8bd9e61099b4d89752bcb543f85b45
mehmettas <mtas372372@hotmail.com>
2019-12-02 01:43:38 +0300

    Base URL Added

---
+++ app/src/main/java/com/app/gamelist/data/remote/service/IGameListService.kt
@@ -0,0 +1,13 @@
+package com.app.gamelist.data.remote.service
+
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IGameListService {
+
+
+
+
+}
--- app/src/main/java/com/app/gamelist/utils/AppConstants.kt
+++ app/src/main/java/com/app/gamelist/utils/AppConstants.kt
@@ -1,4 +1,7 @@
 package com.app.gamelist.utils
 
+import com.app.gamelist.BuildConfig
+
 object AppConstants {
+    const val API_URL = "https://api.rawg.io/api/"
 }
