b515e72b07ecad7e73759337d5a4eafb99d94f40
mehmettas <mtas372372@hotmail.com>
2019-12-02 01:09:31 +0300

    Update viewModelModule - DI for the master page

--- app/src/main/java/com/app/gamelist/di/ViewModelModule.kt
+++ app/src/main/java/com/app/gamelist/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.app.gamelist.di
 
+import com.app.gamelist.ui.main.MainViewModel
 import com.app.gamelist.ui.splash.SplashViewModel
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val viewModelModule = module {
     viewModel { SplashViewModel(get()) }
+    viewModel { MainViewModel(get()) }
 }
